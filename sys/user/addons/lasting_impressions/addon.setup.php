099bc24b12ec3f6c757324b9701df38bb3f7b1f0
Dorothy Molloy <dorothym@climbingturn.co.uk>
2016-06-17 18:47:42 +0100

    Initial commit of ee3 compliant version of Lasting Impressions

---
+++ sys/user/addons/lasting_impressions/addon.setup.php
@@ -0,0 +1,28 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * @package     lasting_impressions
+ * @author      Dorothy Molloy / Anthony Mellor
+ * @link 		http://www.climbingturn.co.uk/software/lasting-impressions-pro
+ * @copyright 	Copyright (c) 2016, Climbing Turn Ltd
+ *
+ *  This file is part of lasting_impressions.
+ *	Requires ExpressionEngine 3.0.0 or above
+ */
+
+
+return array(
+    'author'      => 'Climbing Turn',
+    'author_url'  => 'http://www.climbingturn.co.uk',
+    'name'        => 'Lasting Impressions',
+    'description' => 'Record the entries viewed by each visitor to your site so that you can show the visitor what they last viewed.',
+    'version'     => '4.0.0',
+    'namespace'   => 'ClimbingTurn\LastingImpressions',
+    'settings_exist' => TRUE,
+    'models' => array(
+         'Settings' => 'Model\Settings',
+         'Data' => 'Model\Data'
+    ) 
+
+);
+
